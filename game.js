7fa5713b8cdb766945b71b14d13ac6aa3a6221f1
OlehMospak <alemosag@gmail.com>
2017-11-15 02:55:14 +0200

    kvadrat

--- game.js
+++ game.js
@@ -634,16 +634,11 @@ AlienBullet.prototype = new Bullet();
 
 Asteroid = function () {
   this.init("asteroid",
-            [-10,   0,
-              -5,   7,
-              -3,   4,
-               1,  10,
-               5,   4,
-              10,   0,
-               5,  -6,
-               2, -10,
-              -4, -10,
-              -4,  -5]);
+            [-5,   0,
+            -5, 10,
+			5, 10,
+			5, 0
+			]);
 
   this.visible = true;
   this.scale = 6;
